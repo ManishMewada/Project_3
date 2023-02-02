aef0a342093e92faeb3ddc35bb8e91d9076661e1
ManishMewada <manish.mewada@blobstation.com>
2023-02-02 18:10:31 +0530

    removing files

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var rectangle_width = 200;
+var rectangle_height = 120;
 var rect = document.getElementById("rectangle");
 var left = 100;
 var up = 100;
@@ -27,4 +29,24 @@ function reset() {
     top = 100;
     rectangle_width = 200;
     rectangle_height = 120;
+
+    rect.style.left = left + "px";
+    rect.style.up = up + "px";
+    rect.style.height = rectangle_height + "px";
+    rect.style.width = rectangle_width + "px";
 }
+
+function increaseSize() {
+    rectangle_height += 20;
+    rectangle_width += 20;
+    rect.style.height = rectangle_height + "px";
+    rect.style.width = rectangle_width + "px";
+}
+
+function decreaseSize() {
+    rectangle_height -= 20;
+    rectangle_width -= 20;
+    rect.style.height = rectangle_height + "px";
+    rect.style.width = rectangle_width + "px";
+}
+
